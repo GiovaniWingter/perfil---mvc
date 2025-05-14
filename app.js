59c87e62782027a189306674ae29be385619af89
gigio <Giovani>
2025-05-14 10:49:28 -0300

    ajuste

--- app.js
+++ app.js
@@ -22,5 +22,6 @@ var rotas = require("./app/routes/router");
 app.use("/", rotas);
 
 app.listen(process.env.APP_PORT, () => {
-  console.log(`Servidor online...\nhttp://localhost:${process.env.APP_PORT}`);
+  console.log(`Servidor online...\n
+    http://localhost:${process.env.APP_PORT}`);
 }); 
