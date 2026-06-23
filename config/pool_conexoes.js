03d8ffdea05366f923cadfd7b3879df2d6b08dd2
giovani wingter <giovaniwingter@hotmail.com>
2026-06-23 09:55:55 -0300

    Melhorias no pool

--- config/pool_conexoes.js
+++ config/pool_conexoes.js
@@ -1,18 +1,34 @@
 const mysql = require('mysql2')
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.BD_PORT,
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+});
 
-try {
-    var pool = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
-    });
-    console.log("Conexão estabelecida!");
-} catch (e) {
-    console.log("Falha ao estabelecer a conexão!");
-    console.log(e);
-}
+pool.getConnection((err, conn) => {
+    if (err) {
+        switch (err.code) {
+            case 'ER_ACCESS_DENIED_ERROR':
+                console.error('Usuário ou senha inválidos');
+                break;
 
-module.exports = pool.promise();
+            case 'ECONNREFUSED':
+                console.error('Servidor MySQL não está acessível');
+                break;
+
+            default:
+                console.error(err);
+        }
+    }
+    console.log("Conectado ao SGBD!")
+    conn.release();
+
+});
+
+module.exports = pool.promise()
